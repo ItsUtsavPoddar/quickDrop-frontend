2d03b177f27811f9a6eb276da12114fb59753e77
Utsav Poddar <utsavpoddar002@gmail.com>
2024-06-30 03:56:27 +0530

    1.0.11 fixed Unmounted localStorage call

--- app/page.js
+++ app/page.js
@@ -1,20 +1,28 @@
 "use client";
 import Footer from "@/components/Footer";
 import Chat from "@/components/Chat";
+import { useEffect, useState } from "react";
 
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+  //const [isAuthenticated, setIsAuthenticated] = useState(false);
   const router = useRouter();
 
-  if (localStorage.getItem("token") || localStorage.getItem("guestId")) {
-    return (
-      <main className="text-white font-custMontFont bg-slate-950 ">
-        <Chat />
-      </main>
-    );
-  }
+  useEffect(() => {
+    if (localStorage.getItem("token") || localStorage.getItem("guestId")) {
+      // setIsAuthenticated(true);
+    } else {
+      router.push("/auth");
+    }
+  }, [router]);
 
-  router.push("/auth");
-  return <></>;
+  // if (!isAuthenticated) {
+  //   return null; // Or you can return a loading spinner or a placeholder
+  // }
+  return (
+    <main className="text-white font-custMontFont bg-slate-950 ">
+      <Chat />
+    </main>
+  );
 }
